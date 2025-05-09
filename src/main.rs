8e9affc6e621dc3a951e4d4de41cc3afe63cde4f
jelius-sama <jelius.basumatary.sama@gmail.com>
2025-05-09 21:13:54 +0530

    Added supports for green threads

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::net::TcpListener;
+use rs_routine::{go, init};
+use std::{io, net::TcpListener};
 
 mod api;
 mod libs;
@@ -7,18 +8,38 @@ mod router;
 
 const PORT: &str = ":6969";
 
-fn main() {
+// fn main() {
+//     let listener = TcpListener::bind(format!("0.0.0.0{PORT}")).unwrap();
+//     let pool = libs::ThreadPool::new(69);
+//
+//     println!("Listening on port {PORT}");
+//
+//     for stream in listener.incoming() {
+//         let stream = stream.unwrap();
+//
+//         pool.execute(move || {
+//             router::router(stream);
+//         })
+//         .expect("send client request to the thread pool");
+//     }
+// }
+
+fn main() -> io::Result<()> {
+    init();
+
     let listener = TcpListener::bind(format!("0.0.0.0{PORT}")).unwrap();
-    let pool = libs::ThreadPool::new(69);
 
     println!("Listening on port {PORT}");
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        pool.execute(move || {
-            router::router(stream);
-        })
-        .expect("send client request to the thread pool");
+    for incoming in listener.incoming() {
+        match incoming {
+            Ok(stream) => {
+                // Move the stream into the spawned closure
+                go(move || router::router(stream));
+            }
+            Err(err) => eprintln!("Failed to accept connection: {}", err),
+        }
     }
+
+    Ok(())
 }
